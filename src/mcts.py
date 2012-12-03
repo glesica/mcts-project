f7c2b49f1a9943f9926676fe0061d5e20b15bf5e
George Lesica <glesica@gmail.com>
2012-12-02 17:39:28 -0700

    Implemented the UCT variant. Expanded Node class to compartmentalize some of the work.

--- src/mcts.py
+++ src/mcts.py
@@ -4,6 +4,7 @@ games. Specifically, an implementation of the MCTS algorithm.
 """
 import random, Queue
 from math import sqrt, log
+from random import sample
 
 
 class Game(object):
@@ -172,10 +173,14 @@ class Node(object):
         6: 'purple'
     }
 
-    def __init__(self, parent, action, state, player):
+    def __init__(self, parent, action, state, player, game=None):
+        if parent is None and game is None:
+            raise Exception('No game provided')
+        # Game
+        self.game = game or parent.game
         # Structure
         self.parent    = parent
-        self.children  = {}
+        self.children  = dict.fromkeys(self.game.actions(state))
         # Tree data
         self.action    = action
         self.state     = state
@@ -217,18 +222,107 @@ class Node(object):
             return 0
         return self.value / float(self.visits)
 
+    def search_weight(self, c):
+        """
+        Compute the UCT search weight function for this node. Defined as:
+
+            w = Q(v') / N(v') + c * sqrt(2 * ln(N(v)) / N(v'))
+
+        Where v' is the current node and v is the parent of the current node,
+        and Q(x) is the total value of node x and N(x) is the number of visits
+        to node x.
+        """
+        return self.weight + c * sqrt(2 * log(self.parent.visits) / self.visits)
+
+    def actions(self):
+        """
+        The valid actions for the current node state.
+        """
+        return self.game.actions(self.state)
+
+    def result(self, action):
+        """
+        The state resulting from the given action taken on the current node
+        state by the node player.
+        """
+        return self.game.result(self.state, action, self.player)
+
+    def terminal(self):
+        """
+        Whether the current node state is terminal.
+        """
+        return self.game.terminal(self.state)
+
+    def next_player(self):
+        """
+        Returns the next game player given the current node's player.
+        """
+        return self.game.next_player(self.player)
+
+    def outcome(self, player=None):
+        """
+        Returns the game outcome for the given player (default is the node's
+        player) for the node state.
+        """
+        p = player or self.player
+        return self.game.outcome(self.state, p)
+
+    def fully_expanded(self):
+        """
+        Whether all child nodes have been expanded (instantiated). Essentially
+        this just checks to see if any of its children are set to None.
+        """
+        return not None in self.children.values()
+
+    def expand(self):
+        """
+        Instantiates one of the unexpanded children (if there are any,
+        otherwise raises an exception) and returns it.
+        """
+        try:
+            action = self.children.keys()[self.children.values().index(None)]
+        except ValueError:
+            raise Exception('Node is already fully expanded')
+
+        state = self.game.result(self.state, action, self.player)
+        player = self.game.next_player(self.player)
+
+        child = Node(self, action, state, player)
+        self.children[action] = child
+        return child
+
+    def best_child(self, c=1/sqrt(2)):
+        if not self.fully_expanded():
+            raise Exception('Node is not fully expanded')
+
+        return max(self.children.values(), key=lambda x: x.search_weight(c))
+        
+    def best_action(self, c=1/sqrt(2)):
+        """
+        Returns the action needed to reach the best child from the current
+        node.
+        """
+        return self.best_child(c).action
+
     def max_child(self):
         """
         Returns the child with the highest value.
         """
-        maxc = None
-        maxwt = None
-        for child in self.children.values():
-            if maxwt is None or maxwt < child.weight:
-                maxc = child
-                maxwt = child.weight
-        return maxc
+        return max(self.children.values(), key=lambda x: x.weight)
 
+    def simulation(self, player):
+        """
+        Simulates the game to completion, choosing moves in a uniformly random
+        manner. The outcome of the simulation is returns as the state value for
+        the given player.
+        """
+        st = self.state
+        pl = self.player
+        while not self.game.terminal(st):
+            action = sample(self.game.actions(st), 1)[0]
+            st = self.game.result(st, action, pl)
+            pl = self.game.next_player(pl)
+        return self.game.outcome(st, player)
         
     def dot_string(self, value=False, prettify=lambda x: x):
         """
@@ -258,6 +352,32 @@ class Node(object):
             output += '\t"%s" -> "%s"\n' % (pname, name)
         output += '}'
         return output
+
+
+def mcts_uct(game, state, player, budget):
+    """
+    Implementation of the UCT variant of the MCTS algorithm.
+    """
+    root = Node(None, None, state, player, game)
+    while budget:
+        budget -= 1
+        # Tree Policy
+        child = root
+        while not child.terminal():
+            if not child.fully_expanded():
+                child = child.expand()
+                break
+            else:
+                child = child.best_child()
+        # Default Policy
+        delta = child.simulation(player)
+        # Backup
+        while not child is None:
+            child.visits += 1
+            child.value += delta
+            child = child.parent
+
+    return root.best_action(c=0)
 
 
 def full_tree(game, state, player):
